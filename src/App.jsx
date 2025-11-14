ad79376e285d86eee490900818ef08a0bedd9fcb
Nethsara Vithana <128950446+benja126@users.noreply.github.com>
2025-11-14 21:20:51 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from "./components/Hero"
-import Client from "./components/Client";
-import Protfolio from './components/Protfolio';
-import ContactUs from './components/ContactUs';
+import Client from "./components/Client"
+import Protfolio from './components/Protfolio'
+import ContactUs from './components/ContactUs'
+import { Toaster } from 'react-hot-toast'
+import Footer from './components/Footer'
 
 const App = () => {
 
-  const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.
-  getItem('theme') : 'light')
+  const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
 
   return (
     <div className='dark:bg-black relative'>
+      <Toaster />
       <Navbar theme={theme} setTheme={setTheme} />
        <Hero />
        <Client />
        <Protfolio />
        <ContactUs />
+       <Footer theme={theme} />
+
+    
+
+
     </div>
   )
 }
